55f80e37287b05f018430bb2cf3662829c6cf5ff
Bruce <abraver@livemail.tw>
2020-12-27 20:58:20 +0800

    Go home

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -16,5 +17,17 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    
+
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login request for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title="Sign In", form=form)
+
+
