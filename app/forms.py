74e942a553bf4948261e75d314580e4c05c398c6
Bruce <abraver@livemail.tw>
2020-12-30 16:32:19 +0800

    Finish chapter 8

--- app/forms.py
+++ app/forms.py
@@ -44,3 +44,7 @@ class EditProfileForm(FlaskForm):
             user = User.query.filter_by(username=self.username.data).first()
             if user is not None:
                 raise ValidationError('Please user a different username')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Submit')
